d3a3328be8e88e4e2ca59936eb50dc0946dd4c20
Edanur <edanurgurgen94@gmail.com>
2024-04-23 21:15:39 +0300

    Web services

---
+++ src/main/java/com/example/springboot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.springboot.repository;
+
+import com.example.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
