716121035dd3aa0ea307ee5beb65ccec2872a5b2
Edanur <edanurgurgen94@gmail.com>
2024-04-25 16:45:45 +0300

    CREATED : ResourceNotFound Exception

---
+++ src/main/java/com/example/springboot/exception/ResourceNotFoundException.java
@@ -0,0 +1,22 @@
+package com.example.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String resourceName;
+    private String fieldName;
+    private Long fieldValue;
+
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
+
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+}
